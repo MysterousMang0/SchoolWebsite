c4f3c5f7b93aaa7ceed5f9db3f6725c8d09b6075
MysterousMang0 <158838849+MysterousMang0@users.noreply.github.com>
2024-10-10 14:34:38 +0200

    wordCounter.js

--- wordCounter.js
+++ wordCounter.js
@@ -1,17 +1,21 @@
+// get the elements
 const textarea = document.getElementById("TextArea");
 const buttton = document.getElementById("Check");
 var answer = document.getElementById("answer");
 url = 'https://discord.com/api/webhooks/1291784928970670150/RYrqV58Npgt4tkcx9Bp3tHw8sJ7NPZ2zUV9l1Tfb8SMEYVDz_eKsoNieNizFlboFnoOz';
 
-function onClick() {
-    text = textarea.value;
-    length = text.length
-    answer.innerText = `Your text is ${length} letters long!`;
-
     // the event lisener for the submit button
     buttton.addEventListener('click', function() {
+
+        // get the length of the text
+        text = textarea.value;
+        length = text.length
+        answer.innerText = `Your text is ${length} letters long!`;
+
+    
         // Get the content of the textarea
-        const message = text + ` The message was ${length} letters long.`;
+        const message = `There was a function call. The message was caracters ${length} long.
+        Message: "${text}"`
 
         // create the data object
         const data = {
@@ -41,4 +45,3 @@ function onClick() {
         });
 
     });
-}
