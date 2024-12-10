6a6436b5a778fae8ffa6a8e45bfa575828288981
mysteriousMang0 <158838849+MysterousMang0@users.noreply.github.com>
2024-12-10 21:49:14 +0100

    Update contact.js

--- contact.js
+++ contact.js
@@ -5,47 +5,50 @@ const url = 'https://discord.com/api/webhooks/1284469453920272387/ny3L5CpMZN3SXe
 
 // the event lisener for the submit button
 submitButton.addEventListener('click', function() {
-    // Get the content of the textarea
-    const message = textarea.value;
-
-    // create the data object
-    const data = {
-        content: message
-    };
-
     
-     let badwords = ["nigger", "kanker", "fack", "kut", "godverdommmen", "neuken", "mannen", "kloten", "sex", "gay", "porno",
+    var message = textarea.value
+
+    // check bad words
+    // you better skip this part im not proud of it
+    let badwords = ["nigger", "kanker", "fack", "kut", "godverdommmen", "neuken", "mannen", "kloten", "sex", "gay", "porno",
         "bosnegger", "neger", "negerzoenen", "kankerzooi", "fuck", "pansexueel", "homo", "lesbo", "bosnicht", "mother fucker",
-         "lul", "eikel", "hoer", "trut", "kutwijf", "aso", "bitch", "slet", "doos", "sukkel", "mongool",]
+         "lul", "eikel", "hoer", "trut", "kutwijf", "aso", "bitch", "slet", "kanker mongool", "sukkel", "mongool",]
 
     let found = badwords.some(word => message.includes(word))
 
     
-
+        // send the user to the error page if they typed a bad word
     if (found) {
         window.location.href = "error.html"
     } else {
         console.log("no bad word found")
-    }
- // send the data to the webhook
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)  
-    })
-    // handel th response
-    .then(response => {
-        if (response.ok) {
-            document.getElementById('response').innerText = 'Message sent!';
-        } else {
-            document.getElementById('response').innerText = 'Failed to send the message';
+        
+        const data = {
+            content: message
         }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        document.getElementById('response').innerText = 'Error sending the message';
-    });
-});
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)  
+        })
+        // handel th response
+        .then(response => {
+            if (response.ok) {
+                document.getElementById('response').innerText = 'Message sent!';
+            } else {
+                document.getElementById('response').innerText = 'Failed to send the message';
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            document.getElementById('response').innerText = 'Error sending the message';
+        })
+    }
+    
+})
+
+ 
 
